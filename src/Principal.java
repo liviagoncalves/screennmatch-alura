e2340530850ff1887ce60e1cd83fcc7656d1ac25
Livia Gonçalves <liviagoncalves.morais@gmail.com>
2025-04-21 22:06:02 -0300

    aula 2

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,17 @@
+import br.com.livia.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 2020;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(2020);
+        meuFilme.setDuracaoEmMinutos(180);
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+//        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
